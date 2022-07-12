7b45092712589be3037be10774c02a1a4e7198fb
Misha <46066382+mishashkarubski@users.noreply.github.com>
2022-07-12 16:16:26 +0300

    Task #3: Stabilized bot's behavior by tuning the neural network and adding more convolutional layers
    * Task #3.0.0: Tuned the CNN by adding one more convolutional layer and changing the number of filters of third layer. Changed a training loop. Fixed the normalize_actions() function
    
    * Task #3.0.1: Fixed bug with data logging and removed the excessive 'model.summary()' call
    
    * Task #3: Prevented the CNN from being overfitted by adding two dropout layers with rates 0.2 and 0.3 correspondingly. Improved commits, codestyle, namings and function arguments. Added some more info logging. Implemented the weight saving and loading to reduce the time for model to get ready for performing on real env
    
    * Task #3.1: fixed namings, removed local bugs, improved logging and comments
    
    * Task #3.2: Changed on of the dropout rates to 0.2, removed a jumping action from action space, changed the env from 'MineRLObtainDiamond-v0' to 'MineRLTreechop-v0', re-trained the CNN and saved its weights as another version.
    
    * Task #3.3: Changed the namings, improved comments & code readability
    
    * Task #3.4: minimal fixes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from abc import ABC
-
+import os
 import minerl
 import gym
 import numpy as np
@@ -7,33 +6,36 @@ import tensorflow as tf
 
 from minerl.data import BufferedBatchIter
 from keras import layers, models, optimizers, losses
+from abc import ABC
 
 
+# This class is inherited from the abstract class gym.ActionWrapper that is used to filter out the actions that are not relevant
+# for the current environment.
 class ActionShaper(gym.ActionWrapper, ABC):
-    def __init__(self, env, camera_angle=20):
+    def __init__(self, env, vertical_angle=7.5, horizontal_angle=20):
         super().__init__(env)
 
-        self.camera_angle = camera_angle
+        self.vertical_angle = vertical_angle
+        self.horizontal_angle = horizontal_angle
         self.dataset_actions = [
             [('attack', 1)],
-            [('forward', 1)],
             [('back', 1)],
             [('left', 1)],
             [('right', 1)],
-            [('jump', 1)],
+            [('forward', 1)],
+            [('forward', 1), ('jump', 1), ('sprint', 1)],
             [('forward', 1), ('jump', 1)],
-            # [('forward', 1), ('attack', 1)],
-            [('camera', [-self.camera_angle, 0])],
-            [('camera', [self.camera_angle, 0])],
-            [('camera', [0, self.camera_angle])],
-            [('camera', [0, -self.camera_angle])],
+            [('camera', [-self.horizontal_angle, 0])],
+            [('camera', [self.horizontal_angle, 0])],
+            [('camera', [0, self.vertical_angle])],
+            [('camera', [0, -self.vertical_angle])],
         ]
 
         self.actions = []
         for actions in self.dataset_actions:
             act = self.env.action_space.noop()
-            for a, v in actions:
-                act[a] = v
+            for action, value in actions:
+                act[action] = value
 
             self.actions.append(act)
 
@@ -44,48 +46,50 @@ class ActionShaper(gym.ActionWrapper, ABC):
 
 
 # This function gets the dictionary of actions and returns a numpy array of active actions during each step
-def normalize_actions(actions, batch_size):
+def normalize_actions(actions, batch_size, vertical_padding=7.5, horizontal_padding=5):
     camera_actions = actions["camera"].squeeze()
     attack_actions = actions["attack"].squeeze()
     forward_actions = actions["forward"].squeeze()
+    sprint_actions = actions["sprint"].squeeze()
     back_actions = actions["back"].squeeze()
     left_actions = actions["left"].squeeze()
     right_actions = actions["right"].squeeze()
     jump_actions = actions["jump"].squeeze()
-    batch_size = len(camera_actions)
+
     actions = np.zeros((batch_size,), dtype=np.int)
 
     for i in range(len(camera_actions)):
         # Moving camera has the highest priority
-        if camera_actions[i][0] < -5:
+        if camera_actions[i][0] < -horizontal_padding:
             actions[i] = 7
-        elif camera_actions[i][0] > 5:
+        elif camera_actions[i][0] > horizontal_padding:
             actions[i] = 8
-        elif camera_actions[i][1] > 5:
+        elif camera_actions[i][1] > vertical_padding:
             actions[i] = 9
-        elif camera_actions[i][1] < -5:
+        elif camera_actions[i][1] < -vertical_padding:
             actions[i] = 10
 
-        # Then moving forward with/without jump
-        elif forward_actions[i] == 1:
-            if jump_actions[i] == 1:
-                actions[i] = 6
+        # Then jump with/without moving forward
+        elif jump_actions[i] and forward_actions[i]:
+            if sprint_actions[i]:
+                actions[i] = 5
             else:
-                actions[i] = 1
+                actions[i] = 6
 
-        # Then other navigation actions
-        elif back_actions[i] == 1:
-            actions[i] = 2
-        elif left_actions[i] == 1:
-            actions[i] = 3
-        elif right_actions[i] == 1:
+        # Just move forward if there is no jumping action
+        elif forward_actions[i]:
             actions[i] = 4
 
-        elif jump_actions[i] == 1:
-            actions[i] = 5
+        # Then other navigation actions
+        elif back_actions[i]:
+            actions[i] = 1
+        elif left_actions[i]:
+            actions[i] = 2
+        elif right_actions[i]:
+            actions[i] = 3
 
         # Attacking has the lowest priority
-        elif attack_actions[i] == 1:
+        elif attack_actions[i]:
             actions[i] = 0
         else:
             # No reasonable mapping (will be ignored after applying a mask)
@@ -98,17 +102,26 @@ def main():
     model = models.Sequential([
 
         # First convolutional layer
-        layers.Conv2D(filters=64, kernel_size=(5, 5), activation='relu', input_shape=(64, 64, 3)),
-        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(filters=64, kernel_size=(5, 5), activation='relu', input_shape=(64, 64, 3)),  # Performs a 2D convolution over the input.
+        layers.MaxPooling2D((2, 2)),  # Reduces the size of the input by a factor of 2. This is useful for downsampling the image.
 
         # Second convolutional layer
         layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same'),
         layers.MaxPooling2D((2, 2)),
 
         # Third convolutional layer
-        layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same'),
+        layers.Conv2D(filters=256, kernel_size=(3, 3), activation='relu', padding='same'),
         layers.MaxPooling2D((2, 2)),
+        # Dropout is a regularization technique that randomly drops out units in a neural network. It is used to prevent overfitting.
+        # It disables some neurons in the network with a rate of p. And thus, it generalizes the output.
+        layers.Dropout(0.2),
 
+        # Fourth convolutional layer
+        layers.Conv2D(filters=512, kernel_size=(3, 3), activation='relu', padding='same'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Dropout(0.2),
+
+        # After performing the convolutional layers, the input is flattened to a 1D array and passed to the dense layers.
         layers.Flatten(),
         layers.Dense(units=1024, activation='relu'),
         layers.Dense(units=11, activation='softmax')
@@ -118,30 +131,35 @@ def main():
 
     # Model's parameters
     optimizer = optimizers.Adam(learning_rate=1e-3)  # How fast the model learns
-    loss = losses.SparseCategoricalCrossentropy(from_logits=True)  # How to calculate the loss
+    loss = losses.SparseCategoricalCrossentropy()  # The loss function
     training_metrics = ['accuracy']  # What metrics to track during training
+    checkpoint_path = "weights/adam-v1/adam-v1.ckpt"  # Path to save the weights of the model
+    checkpoint_dir = os.path.dirname(checkpoint_path)  # Directory to later load the weights from
+    # cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True, verbose=1)
 
-    data = minerl.data.make('MineRLTreechop-v0')
-    iterator = BufferedBatchIter(data)
+    # Load the MineRLTreechop-v0 dataset and create a batch iterator
+    # data = minerl.data.make('MineRLTreechop-v0')
+    # iterator = BufferedBatchIter(data)
 
     model.compile(optimizer=optimizer, loss=loss, metrics=training_metrics)
 
     # While iterating through the data, iterator feeds the model with the batches of data in order to train it
-    for state, actions, reward, next_state, done in iterator.buffered_batch_iter(batch_size=16384, num_batches=5):
-        obs = state['pov'].squeeze().astype(np.float) / 255.0
-        actions = normalize_actions(actions, len(obs))
+    # for state, actions, reward, next_state, done in iterator.buffered_batch_iter(batch_size=16384, num_batches=3):
+    #     obs = state['pov'].squeeze().astype(np.float) / 255.0
+    #     actions = normalize_actions(actions, len(obs))
+    #
+    #     # Apply the mask to the observations and actions to make sure the model only sees the relevant data
+    #     mask = actions != -1
+    #     actions = actions[mask]
+    #     obs = obs[mask]
+    #
+    #     model.fit(obs, actions, batch_size=256, epochs=10, verbose=1, callbacks=[cp_callback])
+    #
+    #     print("")
 
-        # Apply the mask to the observations and actions to make sure the model only sees the relevant data
-        mask = actions != -1
-        actions = actions[mask]
-        obs = obs[mask]
-
-        model.fit(obs, actions, batch_size=256, epochs=10, verbose=1)
-
-        print("")
-
-    # Save the model's weights
-    # model.save_weights('./weights/adam-v0')
+    # Load the model's weights
+    latest = tf.train.latest_checkpoint(checkpoint_dir)
+    model.load_weights(latest)
 
     # Use the model to fucking predict the actions
     env = gym.make('MineRLObtainDiamond-v0')
@@ -149,27 +167,27 @@ def main():
 
     obs = env.reset()
 
-    total_reward = 0
+    total_reward, step = 0, 0
     actions_number = env.action_space.n
     action_list = np.arange(actions_number)
+    done = False
 
-    for step in range(18000):
+    while not done:
         env.render()
 
         pov = (obs['pov'].squeeze().astype(np.float) / 255.0).reshape((1, 64, 64, 3))
         action_probabilities = model(pov, training=False)
-
         action = np.random.choice(action_list, p=action_probabilities.numpy()[0])
 
-        obs, reward, done, info = env.step(action)
+        obs, reward, done, _ = env.step(action)
         total_reward += reward
 
-        print("Step: {} Reward: {}".format(step, total_reward))
+        print("Step: {} – Reward: {}".format(step, total_reward))
+        print("Action:", action)
+        print("Number of logs:", obs['inventory']['log'])
         print("")
-        print("Inventory:", obs['inventory']['logs'])
 
-        if done:
-            break
+        step += 1
 
     print("Total reward:", total_reward)
 
