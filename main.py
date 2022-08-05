7aacdda66b5e798305e86e718ce9d00ac552d68c
mishashkarubski <michaelshkarubski@gmail.com>
2022-08-05 13:33:31 +0300

    Task #8: Refactoring in process. Introduced new class that represents MineRL agent, improved comments, etc.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import minerl
 
 from src.Adam import Adam
+from src.Agent import Agent
 from src.OvergroundActionShaper import OvergroundActionShaper
 from src.normalizers import normalize_actions
 from constants.actions import CRAFT_WOODEN_PICKAXE, LOOK_DOWN, CRAFT_STONE_PICKAXE, CRAFT_FURNACE
@@ -10,14 +11,6 @@ from constants.limits import LOGS_TO_CHOP, COBBLESTONE_TO_MINE, IRON_TO_MINE
 
 
 def main():
-    # Create and compile the model
-    model = Adam((64, 64, 3), 11)
-    model.summary()
-    model.compile()
-
-    # Load the model's weights
-    model.load_weights("weights/adam-v2.2/adam-v2.2.ckpt")
-
     # Create the environment to perform the actions on. Currently, the bot uses MineRLObtainDiamond-v0 env
     # because it is the closest env to the original conditions of player in Minecraft survival mode
     env = gym.make('MineRLObtainDiamond-v0')
@@ -27,31 +20,20 @@ def main():
     # Start Minecraft by resetting the environment
     obs = env.reset()
 
+    # Create the model and pass it to MineRL agent, and load the weights from the given path
+    model = Adam((64, 64, 3), 11)
+    agent = Agent(model, obs)
+    agent.load_brain("weights/adam-v2.2/adam-v2.2.ckpt")
+
     # Get the size of the action space and form a list of action indices
-    actions_number = env.action_space.n
-    action_list = np.arange(actions_number)
+    agent.gather_items('log', LOGS_TO_CHOP, env)
 
-    while obs['inventory']['log'] < LOGS_TO_CHOP:
-        env.render()
-
-        pov = (obs['pov'].astype(np.float) / 255.0).reshape(1, 64, 64, 3)
-        # Call the model to predict the actions given the point of view
-        action_probabilities = model(pov)
-        # Apply the probabilities to the action list and sample an action
-        action = np.random.choice(action_list, p=action_probabilities.numpy().squeeze())
-
-        obs, reward, done, info = env.step(action)
-
-    # Unwrap the environment and craft a wooden pickaxe
-    env = env.unwrapped
     craft_wooden_pickaxe = normalize_actions(CRAFT_WOODEN_PICKAXE, env)
     look_down = normalize_actions(LOOK_DOWN, env)
 
     # Perform the actions above and acquire a wooden pickaxe
     # Then once again wrap the environment and prepare for mining. Before that, rotate the camera to look at the ground.
-    for action in craft_wooden_pickaxe + look_down:
-        env.render()
-        obs, reward, done, info = env.step(action)
+    agent.craft_item(craft_wooden_pickaxe + look_down, env)
 
     env = OvergroundActionShaper(env)
 
@@ -69,20 +51,10 @@ def main():
         obs, reward, done, info = env.step(action)
 
     # Wrap the environment, load new weights and find some iron ore.
-    env = OvergroundActionShaper(env)
-    env = gym.wrappers.Monitor(env, './videos/', force=True)
-    model.load_weights("weights/adam-v2.2.0/adam-v2.2.0.ckpt")
+    agent.load_brain("weights/adam-v2.2.0/adam-v2.2.0.ckpt")
+    agent.gather_items('iron_ore', IRON_TO_MINE, env)
 
-    while obs['inventory']['iron_ore'] < IRON_TO_MINE:
-        env.render()
-
-        pov = (obs['pov'].astype(np.float) / 255.0).reshape(1, 64, 64, 3)
-        action_probabilities = model(pov)
-        action = np.argmax(action_probabilities.numpy().squeeze())
-
-        obs, reward, done, info = env.step(action)
-
-    print(obs['inventory'])
+    print(agent.obs['inventory'])
 
 
 if __name__ == '__main__':
