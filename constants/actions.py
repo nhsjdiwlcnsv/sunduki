4ec45d4135b17a3b291313c307a1db8eff42db67
misha-rab-ymniy <93446693+misha-rab-ymniy@users.noreply.github.com>
2022-08-07 13:03:14 +0300

    Finished action opitimization, fixed a bug with done=True and improved bot's performance. (#27)
    * Deleted old ReadME
    
    * Create README.md
    
    * Task #3: Tuned the CNN by adding one more convolutional layer and changing the number of filters of third layer. Changed a training loop. Fixed the normalize_actions() function
    
    * Task #4.1: Fixed bug with data logging and removed the excessive 'model.summary()' call
    
    * Task #3.1: fixed namings, removed local bugs, improved logging and comments
    
    * Task #3: Prevented the CNN from being overfitted by adding two dropout layers with rates 0.2 and 0.3 correspondingly. Improved commits, codestyle, namings and function arguments. Added some more info logging. Implemented the weight saving and loading to reduce the time for model to get ready for performing on real env
    
    * Task #3.2: Changed on of the dropout rates to 0.2, removed a jumping action from action space, changed the env from 'MineRLObtainDiamond-v0' to 'MineRLTreechop-v0', re-trained the CNN and saved its weights as another version.
    
    * Task #4.2: Prepared the code for further actions. Removed useless logging, improved naming and changed the env
    
    * Task #4.2: Prepared the code for further actions. Removed useless logging, improved naming and changed the env
    
    * Prep for task #4: created more variations of the CNN, updated .gitignore to ignore videos.
    
    * Task #4.0: improved naming, introduced a new shaping class to make the env capable of handling crafting, smelting and  placing. Reformated the code
    
    * Task #4: Removed CraftingActionShaper class due to its redundancy. Added a scripted part for crafting a wooden pickaxe and mining some cobblestone. That's a draft commit, however it will be included in the following pull request.
    
    * Task #4.1: Removed useless imports, comments, and logging. Improved bot's preparation for mining. Got rid of the 'Monitor' wrapper. Further comment, code style and readability improvements will be presented after refactoring.
    
    * Task #5: Introduced huge refactoring. Main function is now only responsible for the state machine. Created a new class for the CNN, distinguished classes and functions into separated folders. Improved namings and added more comments.
    
    * Deleted .idea directory
    
    * Deleted .DS_Store file
    
    * Added .DS_Store to .gitignore
    
    * Task #5: Further refactoring performed. Changed comments, revised project configuration, reduced main.py
    
    * Task #6: added an action sequence to craft a stone pickaxe and a furnace.
    
    * Task #7: After crafting stone pickaxes, bot was retrained to act underground. Further improvements needed.
    
    * Craft torch
    
    * Task #6.1: Further changes. Secondary commit.
    
    * lol
    
    * Task #8: Refactoring in process. Introduced new class that represents MineRL agent, improved comments, etc.
    
    * Optimization crafts
    
    * Finished action opitimization, fixed a bug with done=True and improved bot's performance.
    
    Co-authored-by: Misha <46066382+mishashkarubski@users.noreply.github.com>
    Co-authored-by: mishashkarubski <michaelshkarubski@gmail.com>

--- constants/actions.py
+++ constants/actions.py
@@ -2,10 +2,14 @@ from constants.limits import LOGS_TO_CHOP, COBBLESTONE_TO_MINE
 
 CRAFT_WOODEN_PICKAXE = []
 CRAFT_WOODEN_PICKAXE += ['craft:planks'] * LOGS_TO_CHOP
-CRAFT_WOODEN_PICKAXE += ['craft:stick'] * 3
-CRAFT_WOODEN_PICKAXE += ['craft:crafting_table'] * 2
-CRAFT_WOODEN_PICKAXE += ['place:crafting_table'] * 2
-CRAFT_WOODEN_PICKAXE += ['nearbyCraft:wooden_pickaxe']
+CRAFT_WOODEN_PICKAXE += ['craft:stick'] * round(((LOGS_TO_CHOP * 4) - 7) / 2)
+CRAFT_WOODEN_PICKAXE += ['craft:crafting_table']
+CRAFT_WOODEN_PICKAXE += ['camera:[15,0]'] * 30
+CRAFT_WOODEN_PICKAXE += ['attack:1'] * 20
+CRAFT_WOODEN_PICKAXE += ['jump:1'] * 20
+CRAFT_WOODEN_PICKAXE += ['place:crafting_table']
+CRAFT_WOODEN_PICKAXE += ['camera:[-15,0]'] * 30
+CRAFT_WOODEN_PICKAXE += ['craft:wooden_pickaxe']
 CRAFT_WOODEN_PICKAXE += ['equip:wooden_pickaxe']
 
 MINE_DOWN = []
@@ -15,9 +19,9 @@ MINE_DOWN += ['attack:1'] * 500
 CRAFT_STONE_PICKAXE = []
 CRAFT_STONE_PICKAXE += ['camera:[15,0]'] * 30
 CRAFT_STONE_PICKAXE += ['jump:1'] * 20
-CRAFT_STONE_PICKAXE += ['place:crafting_table'] * 2
+CRAFT_STONE_PICKAXE += ['place:crafting_table']
 CRAFT_STONE_PICKAXE += ['camera:[-1,0]']
-CRAFT_STONE_PICKAXE += ['nearbyCraft:stone_pickaxe'] * round((COBBLESTONE_TO_MINE - 8) / 3)
+CRAFT_STONE_PICKAXE += ['craft:stone_pickaxe']
 CRAFT_STONE_PICKAXE += ['equip:stone_pickaxe']
 
 CRAFT_FURNACE = []
